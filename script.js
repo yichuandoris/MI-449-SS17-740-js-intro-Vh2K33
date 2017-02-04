7a04d3e8d80f4c66d0d2e31337c9eac07bdc622f
Yi-ChuanWang <wangyi70@msu.edu>
2017-02-04 14:47:43 -0500

    hopefully to be a final version

--- script.js
+++ script.js
@@ -1,6 +1,32 @@
+var buttonElement = document.getElementById('openhat-button')
+var audioElement = document.getElementById("openhat-audio");
+buttonElement.addEventListener('click', function () {
+  console.log('Just clicked!')
+  audioElement.play();
+})
 
-function playAudio(audioElment) {
-document.getElementById("my-first-button");
-audioElment.source = document.getElementById('openhat.wav');
-        audioElment.play();
-      }
+buttonElement.addEventListener('mouseenter', function () {
+  audioElement.play();
+})
+
+var clapButton = document.getElementById('clap-button')
+var clapAudio = document.getElementById("clap-audio");
+clapButton.addEventListener('click', function () {
+  console.log('Just clicked!')
+  clapAudio.play();
+})
+
+clapButton.addEventListener('mouseenter', function () {
+  clapAudio.play();
+})
+
+var rideButton = document.getElementById('ride-button')
+var rideAudio = document.getElementById("ride-audio");
+rideButton.addEventListener('click', function () {
+  console.log('Just clicked!')
+  rideAudio.play();
+})
+
+rideButton.addEventListener('mouseenter', function () {
+  rideAudio.play();
+})
