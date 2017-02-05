8bc6c6c225148b9ef3f7d35e736d21f0237fa099
Yi-ChuanWang <wangyi70@msu.edu>
2017-02-04 22:50:14 -0500

    updated version based on the comments

--- script.js
+++ script.js
@@ -1,32 +1,47 @@
-var buttonElement = document.getElementById('openhat-button')
-var audioElement = document.getElementById("openhat-audio");
-buttonElement.addEventListener('click', function () {
+//**Openhat**//
+var openhatButton = document.getElementById('openhat-button')
+var openhatAudio = document.getElementById("openhat-audio");
+openhatButton.addEventListener('click', function () {
   console.log('Just clicked!')
-  audioElement.play();
+  openhatAudio.play();
+})
+openhatButton.addEventListener('mouseenter', function () {
+    console.log('Mouseenterred!')
+    openhatAudio.play();
 })
 
-buttonElement.addEventListener('mouseenter', function () {
-  audioElement.play();
-})
-
+//**Clap**//
 var clapButton = document.getElementById('clap-button')
 var clapAudio = document.getElementById("clap-audio");
 clapButton.addEventListener('click', function () {
   console.log('Just clicked!')
   clapAudio.play();
 })
-
 clapButton.addEventListener('mouseenter', function () {
+  console.log('Mouseenterred!')
   clapAudio.play();
 })
 
+//**Ride**//
 var rideButton = document.getElementById('ride-button')
 var rideAudio = document.getElementById("ride-audio");
 rideButton.addEventListener('click', function () {
   console.log('Just clicked!')
   rideAudio.play();
 })
-
 rideButton.addEventListener('mouseenter', function () {
+  console.log('Mouseenterred!')
   rideAudio.play();
+})
+
+//**Tom**//
+var tomButton = document.getElementById('tom-button')
+var tomAudio = document.getElementById("tom-audio");
+tomButton.addEventListener('click', function () {
+  console.log('Just clicked!')
+  tomAudio.play();
+})
+tomButton.addEventListener('mouseenter', function () {
+  console.log('Mouseenterred!')
+  tomAudio.play();
 })
